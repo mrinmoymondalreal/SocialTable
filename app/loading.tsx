8d5767161a2ac8a4ec9d5492de3dfad3b38ff6e5
Mrinmoy Mondal <mrinmoymondalreal@gmail.com>
2024-04-23 15:10:41 +0530

    optimised loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,7 @@
 export default function(){
   return (
-    <main className="mt-4 flex flex-col items-center gap-y-4 px-4">Loading...</main>
+    <main className="mt-4 flex flex-col items-center gap-y-4 px-4">
+      <img src="/loading.gif" className="max-w-full min-w-[300px] h-[calc(100vh-50px)]" alt="" />
+    </main>
   );
 }
